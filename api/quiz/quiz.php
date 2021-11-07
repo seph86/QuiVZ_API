92c4a29d0be8938e306ea99536ccbe2ca804e3fc
Gitea <gitea@fake.local>
2021-11-07 21:00:54 +1000

    Inital testing for pulling quiz data

--- api/quiz/quiz.php
+++ api/quiz/quiz.php
@@ -11,4 +11,15 @@ $functions["quiz"]["start_solo"] = function(&$db) {
   if (!isset($_SESSION["uuid"])) send_data(BAD, "You are not currently logged in");
 
 
+  $json_quiz = file_get_contents("https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple");
+
+  if ($json_quiz === false) {
+    error_log("ERROR: There was an issue connecting to Quiz Server");
+    send_data(BROKEN, "There was an issue connecting to Quiz Server");
+  }
+
+  $data = json_decode($json_quiz, true)["results"];
+
+  send_data(OK, $data);
+
 };
